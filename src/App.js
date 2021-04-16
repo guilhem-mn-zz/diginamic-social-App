cffe547541f8e7d320de5dfd5b8f1189178a2cb7
Poupipomme <poupipomme@gmail.com>
2021-04-16 16:50:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 
 import React from 'react';
 import './App.css';
-import Login from './component/Login';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
-    <Login></Login>
+    <Navbar/>
   );
 }
 
